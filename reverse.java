55b8c6d1e08269a523ce51b0a7f7c0f10fa69db4
shilpa4 <shilpasre91@gmail.com>
2016-07-21 14:19:47 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,13 @@
+
+public class reverse {
+	    public static void main(String [] args){
+	      String s1="Helloworld";
+	   StringBuffer ff=new StringBuffer(s1);
+	 ff.reverse();
+	 String rr=new String(ff);
+
+	    System.out.println(ff);
+	    
+	}
+
+}
